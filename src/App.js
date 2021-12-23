1c3f364ed7fbc9fc70097da44a3bb8a289bc592b
Mounir El-Abbas <eamounir1@gmail.com>
2021-12-23 15:00:54 -0500

    Base code for destinations, need img fix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import Navigation from "./components/navigation/Navigation";
 import Home from "./components/home/Home";
+import Destination from "./components/destination/Destination";
+import Crew from "./components/crew/Crew";
+import Technology from "./components/technology/Technology";
 import logo from "./assets/shared/logo.svg";
+
+import { useAppContext } from "./context";
 const App = () => {
+  const { currentPage } = useAppContext();
   return (
     <div className="App">
       <img id="logo" src={logo} alt="Star shaped logo" />
       <Navigation />
       <div id="page-container">
-        <Home />
+        {currentPage === "home" ? (
+          <Home />
+        ) : currentPage === "destination" ? (
+          <Destination />
+        ) : currentPage === "crew" ? (
+          <Crew />
+        ) : (
+          <Technology />
+        )}
       </div>
     </div>
   );
