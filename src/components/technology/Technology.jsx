d0beb5f6614639c0b1e9733f6ae2bb7b876b748f
Mounir El-Abbas <eamounir1@gmail.com>
2021-12-26 16:32:26 -0500

    Mobile-styles complete for Destination/crew/tech page

--- src/components/technology/Technology.jsx
+++ src/components/technology/Technology.jsx
@@ -12,35 +12,47 @@ const Technology = () => {
       tech => tech.name === currentTechnology
     );
     setTechnology(technologySelected);
-    console.log(`technology`, technology);
   }, [currentTechnology]);
 
   return (
     <>
       {Object.keys(technology).length ? (
         <div className="technology-page page">
-          <h5 className="subheading-sm">Space Launch 101</h5>
-          <picture>
-            <source
-              media="(min-width: 1440px)"
-              srcSet={technology.images.portrait}
-            />
-            <img src={technology.images.landscape} alt="" />
-          </picture>
-          <div>
-            <ul>
-              {data.technology.map(tech => (
-                <li onClick={e => changeTechnology(e)} name={tech.name}>
-                  {tech.name}
-                </li>
-              ))}
-            </ul>
-            <div id="technology-info">
-              <h5 className="subheading-sm">The Terminology...</h5>
-              <h2 className="subheading-lg">{technology.name}</h2>
-              <p className="page-description">{technology.description}</p>
-            </div>
-          </div>
+          <header className="page-header">
+            <h5>
+              <span>03</span>Space Launch 101
+            </h5>
+          </header>
+          <main>
+            <picture>
+              <source
+                media="(min-width: 1440px)"
+                srcSet={technology.images.portrait}
+              />
+              <img id="tech-pic" src={technology.images.landscape} alt="" />
+            </picture>
+            <section>
+              <ul>
+                {data.technology.map((tech, index) => (
+                  <li
+                    className={`${
+                      currentTechnology === tech.name
+                        ? "current-technology"
+                        : ""
+                    }`}
+                    onClick={e => changeTechnology(e)}
+                    name={tech.name}>
+                    {index + 1}
+                  </li>
+                ))}
+              </ul>
+              <div className="page-info">
+                <h5 className="subheading-bc-b">The Terminology...</h5>
+                <h2>{technology.name}</h2>
+                <p>{technology.description}</p>
+              </div>
+            </section>
+          </main>
         </div>
       ) : (
         <h2>Loading...</h2>
